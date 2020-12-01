c7557b01954688cf8dfa34f7e09fb7884346efbe
LEEHEEJOON <marco920322@gmail.com>
2020-12-02 00:11:15 +0900

    Modify promise-to-async

--- async/13_promise-to-async.js
+++ async/13_promise-to-async.js
@@ -1,21 +1,29 @@
 class UserStorage {
-  async loginUser(id, password) {
-    if (
-      (id === 'ellie' && password === 'dream') ||
-      (id === 'coder' && password === 'academy')
-    ) {
-      return id;
-    } else {
-      throw new Error('not found');
-    }
+  loginUser(id, password) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (
+          (id === 'ellie' && password === 'dream') ||
+          (id === 'coder' && password === 'academy')
+        ) {
+          resolve(id);
+        } else {
+          reject(new Error('not found'));
+        }
+      }, 2000);
+    });
   }
 
-  async getRoles(id) {
-    if (id === 'ellie') {
-      return { name: 'ellie', role: 'admin' };
-    } else {
-      throw new Error('no access');
-    }
+  getRoles(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user === 'ellie') {
+          resolve({ name: 'ellie', role: 'admin' });
+        } else {
+          reject(new Error('no access'));
+        }
+      }, 1000);
+    });
   }
 }
 
@@ -23,8 +31,10 @@ const userStorage = new UserStorage();
 const id = prompt('enter your id');
 const password = prompt('enter your password');
 
-userStorage
-  .loginUser(id, password)
-  .then((id) => userStorage.getRoles(id))
-  .then((user) => alert(`Hello ${user.name}, you have a ${user.role} role`))
-  .catch((error) => console.log(error));
+async function practiceAsync() {
+  const login = await userStorage.loginUser(id, password);
+  const user = await userStorage.getRoles(login);
+  return alert(`Hello ${user.name}, you have a ${user.role} role`);
+}
+
+practiceAsync().then(console.log);
